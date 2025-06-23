e159b36a2231f0c73584b502f21112a4956ba661
ADEMILSON PIRES MIRANDA SÁ <89095980+ademilsonpires@users.noreply.github.com>
2025-06-22 21:03:51 -0300

    Update config_adm.py

--- models/config_adm.py
+++ models/config_adm.py
@@ -1,9 +1,15 @@
 # Configurações do banco de dados
+#variaveis ambiente dev
+# DB_USER = "root"
+# DB_PASSWORD = ""
+# DB_HOST = "localhost"
+# DB_NAME = "sysfit_adm"
+#variaveis ambiente producao
+DB_USER = "slzdominios_webmoney"
+DB_PASSWORD = "VYcanismajoris1#"
+DB_HOST = "31.220.52.31"
+DB_NAME = "slzdominios_sysfit_adm"
 
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_NAME = "sysfit_adm"
 
 # Configurações fixas de acesso ao MySQL e dump
 MYSQL_USER = "root"
